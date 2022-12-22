70c57e05da3153ab9851f66e20a9c1436d64c2b8
Robert Parker <robbybparker@gmail.com>
2022-12-21 21:49:34 -0700

    start including code into JuMPIn.jl

--- src/JuMPIn.jl
+++ src/JuMPIn.jl
@@ -2,4 +2,8 @@ module JuMPIn
 
 # Write your package code here.
 
+# Methods to identify variables
+include("identify_variables.jl") # IdentifyVariables
+identify_variables = IdentifyVariables.identify_unique_variables
+
 end
