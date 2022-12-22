3a2f6c033a8f8cb47b0f7741126f1b8c852bdcd8
Robert Parker <robbybparker@gmail.com>
2022-12-22 11:25:05 -0700

    add get_equality_constraints to JuMPIn module

--- src/JuMPIn.jl
+++ src/JuMPIn.jl
@@ -5,4 +5,8 @@ module JuMPIn
 include("identify_variables.jl") # IdentifyVariables
 identify_unique_variables = IdentifyVariables.identify_unique_variables
 
+# Methods to identify equality constraints
+include("get_equality.jl") # GetEquality
+get_equality_constraints = GetEquality.get_equality_constraints
+
 end
