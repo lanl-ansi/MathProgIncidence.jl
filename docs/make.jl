ce0c3e463bb00a6a31d34bf63a0b5e1a11fabc36
Robert Parker <robbybparker@gmail.com>
2022-12-29 19:01:15 -0700

    start API reference docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,4 +4,15 @@ using JuMPIn
 makedocs(
     sitename = "JuMPIn",
     format = Documenter.HTML(prettyurls = false),
+    pages = [
+        "Introduction" => "index.md",
+        "Overview" => "overview.md",
+        "Simple Example" => "example.md",
+        "API Reference" => [
+            "reference/get_equality.md",
+            "reference/identify_variables.md",
+            "reference/incidence_graph.md",
+            "reference/interface.md",
+        ],
+    ],
 )
---
+++ docs/src/reference/get_equality.md
@@ -0,0 +1,12 @@
+# GetEquality
+
+```@meta
+CurrentModule = JuMPIn
+```
+
+```@docs
+GetEquality
+GetEquality.get_equality_constraints
+GetEquality.is_equality
+GetEquality.set_implies_equality
+```
