c0d6453d59ef54063232460e8a30c36e1046a37d
robbybp <robbybparker@gmail.com>
2023-07-15 16:57:52 -0600

    add test with fixed variable

--- test/get_equality.jl
+++ test/get_equality.jl
@@ -18,7 +18,7 @@
 #  ___________________________________________________________________________
 
 module TestGetEquality
-import JuMP as jmp
+import JuMP
 import MathOptInterface as moi
 using Test: @test, @test_throws
 using JuMPIn: get_equality_constraints
@@ -28,9 +28,9 @@ include("models.jl") # Models
 function get_flow_model_with_inequalities()
     m = Models.make_degenerate_flow_model()
     # TODO: Some assertions with the @assert macro
-    @jmp.constraint(m, ineq1, m[:x][1] >= 0)
-    @jmp.constraint(m, ineq2, m[:x][1]^2 + m[:x][2]^2 <= 0.5)
-    @jmp.NLconstraint(m, ineq3, sqrt(m[:x][3]) >= 0.1)
+    @JuMP.constraint(m, ineq1, m[:x][1] >= 0)
+    @JuMP.constraint(m, ineq2, m[:x][1]^2 + m[:x][2]^2 <= 0.5)
+    @JuMP.NLconstraint(m, ineq3, sqrt(m[:x][3]) >= 0.1)
     return m
 end
 
@@ -42,15 +42,24 @@ function test_flow_model_with_inequalities()
 end
 
 function test_with_vector_constraint()
-    m = jmp.Model()
-    @jmp.variable(m, var[1:2])
-    @jmp.constraint(m, con, var in moi.Nonnegatives(2))
+    m = JuMP.Model()
+    @JuMP.variable(m, var[1:2])
+    @JuMP.constraint(m, con, var in moi.Nonnegatives(2))
     @test_throws(TypeError, eq_cons = get_equality_constraints(m))
+end
+
+function test_with_fixed_variables()
+    m = JuMP.Model()
+    @JuMP.variable(m, x[1:2])
+    JuMP.fix(x[1], 1.0)
+    eq_cons = get_equality_constraints(m)
+    @test length(eq_cons) == 1
 end
 
 function runtests()
     test_flow_model_with_inequalities()
     test_with_vector_constraint()
+    test_with_fixed_variables()
 end
 
 end # module TestGetEquality
