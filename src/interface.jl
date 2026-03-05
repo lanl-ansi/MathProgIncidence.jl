c9a913af2ff64c3fc416a4c08c7f4a039590ae16
Robert Parker <robbybparker@gmail.com>
2026-03-04 19:03:23 -0500

    typo

--- src/interface.jl
+++ src/interface.jl
@@ -556,7 +556,7 @@ connected_components(matrix::Matrix) = connected_components(IncidenceGraphInterf
 const Subsystem = NamedTuple{
     (:con, :var),
     # NOTE: These fields are not fully typed as we need to support ints as
-    # wells as variables/constraints.
+    # well as variables/constraints.
     # TODO: Parameterize this type? Or just always use ints, then keep the vars/cons
     # somewhere else?
     Tuple{Vector,Vector},
